5c7dec7327a45c040d22f7e5f0f0d98910020cec
ankur-mukherjee2003 <120292298+ankur-jod@users.noreply.github.com>
2023-11-13 22:32:13 +0530

    yes

--- 1st/first.cpp
+++ 1st/first.cpp
@@ -1,7 +1,39 @@
-#include<iostream>
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
-int main(){
-    
-    cout<<59%7;
+
+int binarySearch(vector<int> arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9};
+    int target = 5;
+
+    int result = binarySearch(arr, target);
+
+    if (result == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at index " << result << endl;
+    }
+
     return 0;
 }
