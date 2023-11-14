74753719760ccb2c311a6920e15c410af82dcfe1
ankur-mukherjee2003 <120292298+ankur-jod@users.noreply.github.com>
2023-11-14 10:08:06 +0530

    yes

---
+++ array/array_function.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void inputArray(int arr[], int size) {
+    cout << "Enter " << size << " integers: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int size) {
+    cout << "Array elements: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[MAX_SIZE];
+    int size;
+
+    cout << "Enter size of array: ";
+    cin >> size;
+
+    inputArray(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
