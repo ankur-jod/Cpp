030f5bad6e5521b8fbc1248802e3ef4494551ffd
ankur-mukherjee2003 <120292298+ankur-jod@users.noreply.github.com>
2023-11-28 21:23:53 +0530

    yes

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -6,9 +6,9 @@ void bubbleSort(int arr[], int n) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j]=arr[j]^arr[j+1];
+                arr[j+1]=arr[j]^arr[j+1];
+                arr[j]=arr[j]^arr[j+1];
             }
         }
     }
